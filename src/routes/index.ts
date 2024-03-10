ae4f9f1dab754d390c295e0bf4d54d2c15316c63
Nabil AbuShaban <nabil.abushaban@gmail.com>
2024-03-10 13:46:14 +0200

    add logs

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -7,12 +7,14 @@ const router = Router();
 router.use('/stripe', stripe)
 
 router.use((req, res) => {
+  console.log(`you have requested ${req.url}`)
   res.status(404).json({
     status: 404,
     message: 'Not found',
   });
 });
 router.use((err: any, req: Request, res: Response) => {
+  console.log(`you have requested ${req.url}`)
   throw new GenericError(err, 500);
 });
 
